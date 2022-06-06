2ad016c1b2a666b66b73f1d74b10ea98f3a3dcad
Sy <jf797@student.aru.ac.uk>
2022-06-06 23:14:08 +0100

    master

---
+++ main.py
@@ -0,0 +1,50 @@
+# imports
+import discord
+import asyncio
+
+client = discord.Client()
+
+TOKEN = ""
+
+linkFound = False
+
+@client.event
+async def on_ready():
+    print(f"logged in as {client.user.name}")
+    print(f"with user ID: {client.user.id}")
+
+def convertVmToTrue(message):
+    rawUserMessage = str(message.content)
+    videoLink = rawUserMessage.replace("vm.", "")
+    return videoLink
+
+def downloadLink(link):
+    return
+
+
+
+
+def checkForLink(message):
+    userMessage = str(message.content)
+    linkSearchCriteria = "vm.tiktok.com"
+
+    if linkSearchCriteria in userMessage:
+        print('link found')
+        linkToDownload = convertVmToTrue(message)
+    else:
+        return
+
+
+# logging
+@client.event 
+async def on_message(message):
+    username = str(message.author).split('#')[0]
+    userMessage = str(message.content)
+    channelMessage = str(message.channel.name)
+    print(f'User:{username}: Content:{userMessage} | In:{channelMessage}')
+
+    if message.author == client.user:
+        return
+    checkForLink(message)
+
+client.run(TOKEN)
