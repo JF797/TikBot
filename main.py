a26d0c8eb17ff5ce6e41adcda6f8c463d886be0f
Sy <jf797@student.aru.ac.uk>
2022-06-21 22:38:25 +0100

    Progress - downloader now works

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 # imports
 import discord
 import asyncio
+import requests
+import wget
+import re
 
 client = discord.Client()
 
@@ -13,14 +16,49 @@ async def on_ready():
     print(f"logged in as {client.user.name}")
     print(f"with user ID: {client.user.id}")
 
+def splitLink(url):
+    return url.split('?')[0]
+
+
 def convertVmToTrue(message):
-    rawUserMessage = str(message.content)
-    videoLink = rawUserMessage.replace("vm.", "")
+    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; rv:91.0) Gecko/20100101 Firefox/91.0"}
+
+    url = message
+    response = requests.get(url, headers=headers)
+
+    videoLink = response.url
+    print(videoLink) # the correct url with the username
     return videoLink
 
+def getVideoLink(link):
+    url = "https://tiktok-info.p.rapidapi.com/dl/"
+    querystring = {"link":link}
+    headers = {
+        "X-RapidAPI-Key": "MYAPIKEY",
+        "X-RapidAPI-Host": "tiktok-info.p.rapidapi.com"
+    }
+    response = requests.request("GET", url, headers=headers, params=querystring)
+    print(response.text)
+    url = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', response.text)
+    print(url)
+    url = (url[0])
+    return url
+
+
 def downloadLink(link):
+    outputDirectory = "./tmp.mp4"
+    wget.download(link, out=outputDirectory)
     return
 
+def linkRequest(link):
+    url = "https://tiktok-info.p.rapidapi.com/dl/"
+    querystring = {"link":link}
+    headers = {
+        "X-RapidAPI-Key": "MYAPIKEY",
+        "X-RapidAPI-Host": "tiktok-info.p.rapidapi.com"
+    }
+    response = requests.request("GET", url, headers=headers, params=querystring)
+    print(response.text)
 
 
 
@@ -30,10 +68,17 @@ def checkForLink(message):
 
     if linkSearchCriteria in userMessage:
         print('link found')
-        linkToDownload = convertVmToTrue(message)
+        # convert vm link to true link
+        linkToDownload = convertVmToTrue(userMessage)
+        # splits out the bit we don't need (after the '?')
+        linkToDownload = splitLink(linkToDownload)
+        print(linkToDownload)
+        # gets the .mp4 link via the api
+        linkToDownload = getVideoLink(linkToDownload)
+        # download the provided .mp4 link 
+        downloadLink(linkToDownload)
     else:
         return
-
 
 # logging
 @client.event 
